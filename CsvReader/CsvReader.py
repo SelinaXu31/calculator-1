cbda0c1b3291ab8290341963978436c47c4390d3
SelinaXu31 <65613560+SelinaXu31@users.noreply.github.com>
2021-07-10 21:23:30 -0400

    Update CsvReader.py
    Add the absolute  path function

--- CsvReader/CsvReader.py
+++ CsvReader/CsvReader.py
@@ -11,7 +11,8 @@ class CsvReader:
 
     def __init__(self, filepath):
         self.data = []
-
+        relative = Path(filepath)
+        absolute = relative.absolute()
         with open(absolutepath(filepath)) as text_data:
             csv_data = csv.DictReader(text_data, delimiter=',')
             for row in csv_data:
